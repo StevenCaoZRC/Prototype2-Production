07d68756dbb7c119e9ca73997a0e962b839584e5
Vivian <seikyura@gmail.com>
2019-03-11 12:21:34 +1300

    Simple move + Terrain color
    Very very simple movement and color change in terrain

---
+++ Prototype2/Assets/TempCharaMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempCharaMove : MonoBehaviour
+{
+    public float m_speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movecharacter();
+    }
+
+    void Movecharacter()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 position = new Vector3(hor, 0, ver) * m_speed * Time.deltaTime;
+
+        transform.Translate(position);
+    }
+}
