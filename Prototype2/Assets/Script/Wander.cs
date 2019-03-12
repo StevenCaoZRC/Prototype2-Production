36b82f8aab27c3a5adcee0c22ad580f1b7ee1aea
Vivian <seikyura@gmail.com>
2019-03-13 12:07:40 +1300

    Patrol and wait

--- Prototype2/Assets/Script/Wander.cs
+++ Prototype2/Assets/Script/Wander.cs
@@ -1,21 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Wander : MonoBehaviour
 {
-    protected float m_wanderTime = 3.0f; //Wander time before they switch directions
-    protected float m_movementSpeed = 0.1f;
+    private float m_movementSpeed = 30.0f;
+    private float m_startWaitTime = 3.0f; //Wander time before they switch directions
+    private float m_waitTime = 10.0f; //Wander time before they switch directions
+
+    private Transform m_initPosition;
+    public Transform m_destination;
+
+    NavMeshAgent m_navMeshAgent;
 
     // Start is called before the first frame update
     void Start()
     {
+         m_navMeshAgent = this.GetComponent<NavMeshAgent>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        SetDestination();
+    }
+
+    private void SetDestination()
+    {
+        if(m_destination != null)
+        {
+            Vector3 target = m_destination.transform.position;
+            m_navMeshAgent.SetDestination(target);
+            m_navMeshAgent.stoppingDistance = 10.0f;
+        }
     }
 }
---
+++ Prototype2/Assets/Script/WayPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    [SerializeField]
+    private float m_radius = 1.0f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, m_radius);
+    }
+}
