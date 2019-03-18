59c0d13832435a01f85f2f9993f55253fe2b713e
Steven <stevenzhaooooo@gmail.com>
2019-03-18 17:49:51 +1300

    Gonna move on from other stuff

--- Prototype2/Assets/Script/CameraMovement.cs
+++ Prototype2/Assets/Script/CameraMovement.cs
@@ -30,44 +30,46 @@ public class CameraMovement : MonoBehaviour
         m_camStartPos = new Vector3(0, 4, -6);
         //m_resetCamPos.transform.position = new Vector3(0, 2f, -4);
         //rotation = Quaternion.Euler(15, 0, 0);
-        m_camTransform.rotation = Quaternion.Euler(15, 0, 0);
+       // m_camTransform.rotation = Quaternion.Euler(15, 0, 0);
         m_startingRotation = m_camTransform.rotation;
 
     }
 
     private void Update()
     {
+        
         m_currentX += Input.GetAxis("cameraRotHor");
         m_currentY += Input.GetAxis("cameraRotVer");
 
         m_currentY = Mathf.Clamp(m_currentY, Y_MIN_ANGLE, Y_MAX_ANGLE);
         // m_currentX = Mathf.Clamp(m_currentX, X_MIN_ANGLE, X_MAX_ANGLE);
-        
+   
     }
 
         // Update is called once per frame
     void LateUpdate()
     {
-        if (Input.GetButton("CamReset"))
-        {
-            Quaternion rotation1 = Quaternion.Euler(m_resetCamPos.rotation.x, m_resetCamPos.rotation.y, m_resetCamPos.rotation.z);
-            m_camTransform.position = rotation1 * m_resetCamPos.position;
-            m_camTransform.LookAt(m_lookAt.position);
-
-            m_currentX = 0.0f;
-            m_currentY = 0.0f;
-            return;
-        }
+      
         CamMovement();
 
     }
     private void CamMovement()
     {
+      
 
         
+        if (Input.GetButtonDown("CamReset"))
+        {
+
+            m_currentX = 0.0f;
+            m_currentY = 0.0f;
+            rotation = Quaternion.Euler(m_currentY, m_currentX, 0);
+            m_camTransform.position =  rotation * m_camStartPos;
+            m_camTransform.LookAt(m_lookAt.position);
+        }
         rotation = Quaternion.Euler(m_currentY, m_currentX, 0);
-            m_camTransform.position = m_lookAt.position + rotation * m_camStartPos;
-            m_camTransform.LookAt(m_lookAt.position );
+        m_camTransform.position = m_lookAt.position + rotation * m_camStartPos;
+        m_camTransform.LookAt(m_lookAt.position );
 
 
 
