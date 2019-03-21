18bb3807ac265f419c550804178ff89a5c3151d9
Steven <stevenzhaooooo@gmail.com>
2019-03-21 16:31:06 +1300

    defeated by camera reset location gg

--- Prototype2/Assets/Script/CameraMovement.cs
+++ Prototype2/Assets/Script/CameraMovement.cs
@@ -6,9 +6,10 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform m_cameraPivot;
     public GameObject m_player;
+    public Transform m_ResetLoc;
 
     private const float Y_MIN_ANGLE = -35.0f;
-    private const float Y_MAX_ANGLE = 60.0f;
+    private const float Y_MAX_ANGLE = 40.0f;
 
     public float m_sensitivity = 1.0f;
 
@@ -37,21 +38,22 @@ public class CameraMovement : MonoBehaviour
         m_currentY = Mathf.Clamp(m_currentY, Y_MIN_ANGLE, Y_MAX_ANGLE);
 
         transform.LookAt(m_cameraPivot);
-    
-        if (m_currentX != 0)
-        {
-            m_cameraPivot.rotation = Quaternion.Euler(m_currentY, m_currentX, 0);
-        }
 
-        if (Input.GetButtonDown("CamReset"))
+        if (m_currentX != 0 && m_currentY != 0)
         {
-            m_currentX = 0;
-            m_currentY = 0;
-
-            m_cameraPivot.rotation = Quaternion.Euler(m_player.transform.rotation.x, m_player.transform.rotation.y, 0);
-           // m_cameraPivot.position = m_player.transform.GetChild(0).transform.position;
-            m_cameraPivot.forward = m_player.transform.forward;
-            
+            m_cameraPivot.rotation = Quaternion.Euler(m_currentY,  m_currentX, 0);
         }
+        //if (Input.GetButtonDown("CamReset"))
+        //{
+        //   m_currentX = 0;
+        //   m_currentY = 0;
+        //   // m_cameraPivot.rotation = Quaternion.Euler(m_player.transform.rotation.x, m_player.transform.rotation.y, 0);
+        //   //// transform.rotation = Quaternion.Euler(m_player.transform.rotation.x, m_player.transform.rotation.y, 0); 
+        //    m_cameraPivot.forward = m_player.transform.forward;
+        //   // transform.position = m_ResetLoc.position;
+        //    m_cameraPivot.rotation = Quaternion.Euler(m_cameraPivot.rotation.x , m_cameraPivot.rotation.y + 2.5f, m_cameraPivot.rotation.z - 5);
+        //    //m_cameraPivot.position = new Vector3(m_player.transform.position.x, m_player.transform.position.y + 2.5f, m_player.transform.position.z);
+
+        //}
     }
 }
